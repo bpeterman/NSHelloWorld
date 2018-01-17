787610d1abcfe3aaf9ba47803b30c9c6f1c947a8
Blake Peterman <blake.peterman@viasat.com>
2018-01-17 16:01:34 -0700

    initial commit

---
+++ app/main.ts
@@ -0,0 +1,5 @@
+import "./bundle-config";
+import { platformNativeScriptDynamic } from "nativescript-angular/platform";
+import { AppModule } from "./app.module";
+
+platformNativeScriptDynamic().bootstrapModule(AppModule);
